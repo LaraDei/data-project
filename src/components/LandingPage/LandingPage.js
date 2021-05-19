19a10eab5b055055cdf61639f2cfee0c35d8e0d3
Lara Samuelson <lara.samuelson@gmail.com>
2021-05-19 10:07:00 -0700

    initial Commit

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LandingPage() {
+    return (
+        <div className="landing-page">
+            <p>Create your own data project</p>
+        </div>
+    )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from'@fortawesome/free-solid-svg-icons';
+import './NavBar.css'
+
+export default function NavBar() {
+    return(
+        <div className="nav-bar">
+          <div className='icon-wrapper'>
+        {/* <button className='icon' id='nav-button' onClick={e => this.handleNav()}><FontAwesomeIcon icon={faBars}/>{' '}</button> */}
+        </div>
+        <ul id='menu' className='menu'>
+            <li className="logo"><Link to={'/'}>Data Project</Link></li>
+            <li><Link to={'/tutorial'}>Tutorial</Link></li>
+            <li><Link to={'/sign-in'}>Sign In/Out</Link></li>
+            <li><Link to={'/sign-up'}>Create Account</Link></li>
+        </ul>
+        
+        </div>
+    )
+}
